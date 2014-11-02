b1db27b4e35b451314f93d29f0d3b1511cccf6b5
SeymaYildirim <seyma.yildirim@bil.omu.edu.tr>
2014-11-02 23:31:07 +0200

    Create RandomName.js

---
+++ RandomName.js
@@ -0,0 +1,71 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+	
+</style>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript">
+    
+    var words = {
+                "TR" :
+                {
+                "names" :
+                [
+                "masa"
+                "kitap"
+                "kalem"
+                "kulaklık"
+                "elbise"
+                "kumsal"
+                "rüya"
+                "gitar"
+                "araba"
+                "telefon"
+                ]
+                "adjectives" :
+                [
+                "engebeli"
+                "çirkin"
+                "şişman"
+                "kirli"
+                "üzgün"
+                "mutlu"
+                "iğrenç"
+                "tatlı"
+                "ekşi"
+                "nemli"
+                ]
+                "EN" :
+                {
+                "names"
+                [
+                "desk"
+                "book"
+                "pencil"
+                "headphone"
+                "dress"
+                "beach"
+                "dream"
+                "guitar"
+                "car"
+                "telephone"
+                ]
+                "adjectives" :
+                "rude"
+                "ugly"
+                "fat"
+                "dirty"
+                "sad"
+                "happy"
+                "disgusting"
+                "tasty"
+                "sour"
+                "wet"
+                ]
+                }
+                }
+                }
+                
+                
+                
